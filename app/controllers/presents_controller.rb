5f5290a81ac1a548dd67acd9399a718d8a810807
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-14 01:35:17 +0800

    View for showing present verb complete

--- app/controllers/presents_controller.rb
+++ app/controllers/presents_controller.rb
@@ -1,2 +1,25 @@
 class PresentsController < ApplicationController
+	before_action :find_verb
+
+	def new  
+		@present = @verb.presents.new
+	end
+
+	private
+
+	def find_verb
+		@verb = Verb.find(params[:verb_id])
+	end
+
+	def present_params
+		params.require(:present).permit(
+			:first_person,
+			:second_person,
+			:third_person,
+			:plural_first_person,
+			:plural_second_person,
+			:plural_third_person
+		)
+	end
+
 end
