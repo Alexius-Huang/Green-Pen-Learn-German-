6db9af23cec91ab5d1d527cd342138f15831a95c
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-14 00:35:29 +0800

    Verb RESTful routes and controller setup

--- app/controllers/verb_controller.rb
+++ app/controllers/verb_controller.rb
@@ -1,2 +1,52 @@
 class VerbController < ApplicationController
+
+	def index
+		@verb = Verb.all
+	end
+
+	def new
+		@verb = Verb.new
+	end
+
+	def create
+		@verb = Verb.create(verb_params)
+
+		if @verb.save
+			redirect_to verbs_path, notice: "New Verb Successfully Created!"
+		else
+			render :new
+		end
+	end
+
+	def show
+		@verb = Verb.find(params[:id])
+	end
+
+	def edit
+		@verb = Verb.find(params[:id])
+	end
+
+	def update
+		@verb = Verb.find(params[:id])
+
+		if @verb.update(verb_params)
+			redirect_to verb_path(@verb), notice: "Verb : #{@verb.title} has Successfully Updated!"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@verb = Verb.find(params[:id])
+		verb_title = @verb.title
+		@verb.destroy
+		redirect_to verbs_path, alert: "Verb : #{verb_title} has Successfully Deleted!"
+	end
+
+	private
+
+	def verb_params
+		params.require(:verb).permit(:title, :description)
+	end
+
 end
