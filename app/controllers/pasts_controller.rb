af1ea64868b6e86d56dcac1cb1c23a40171634ee
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-14 21:35:25 +0800

    Setup past controller's actions

--- app/controllers/pasts_controller.rb
+++ app/controllers/pasts_controller.rb
@@ -1,2 +1,70 @@
 class PastsController < ApplicationController
+	before_action :find_verb
+
+	def new  
+		@past = @verb.pasts.new
+		@personalpronomen = [
+			"first_person", 
+			"second_person", 
+			"third_person",
+			"plural_first_person",
+			"plural_second_person",
+			"plural_third_person",
+			"description"
+		 ]
+		@grammer = [ "ich", "du", "er/es/sie", "wir", "ihr", "Sie/sie", "Further Explaination"]
+	end
+
+	def create
+		@past = @verb.pasts.build(past_params)
+
+		if @past.save
+			redirect_to verb_path(@verb), notice: "Verb : #{@verb.title} has Successfully Updated Information!"
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@past = @verb.pasts.last
+		@personalpronomen = [
+			"first_person", 
+			"second_person", 
+			"third_person",
+			"plural_first_person",
+			"plural_second_person",
+			"plural_third_person",
+			"description"
+		 ]
+		@grammer = [ "ich", "du", "er/es/sie", "wir", "ihr", "Sie/sie", "Further Explaination"]
+	end
+
+	def update
+		@past = @verb.pasts.last
+
+		if @past.update(past_params)
+			redirect_to verb_path(@verb), notice: "Verb : #{@verb.title} has Successfully Updated Information!"
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def find_verb
+		@verb = Verb.find(params[:verb_id])
+	end
+
+	def present_params
+		params.require(:past).permit(
+			:first_person,
+			:second_person,
+			:third_person,
+			:plural_first_person,
+			:plural_second_person,
+			:plural_third_person,
+			:description
+		)
+	end
+
 end
