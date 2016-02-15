dd4b3ed1b3204944a3a2f39ca7e2ce8a4cb07214
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-15 22:40:08 +0800

    Able to auto-generate first verb's content

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,21 +37,41 @@ should_generate_5_german_verbs = (0..4).each do |number|
 	Verb.create(title: sample_german_verbs[number], description: sample_verb_description[number])
 end
 
-print "Complete!"
+puts "Complete!"
 
+print "Seed file generating verb tenses content for the first verb..."
 
+sample_present_tense = ["habe", "hast", "hat", "haben", "habt", "haben", "Irregular german verb."]
+sample_past_tense = ["hatte", "hattest", "hatte", "hatten", "hattet", "hatten", "Irregular german verb."]
+sample_perfect_tense = ["habe gehabt", "hast gehabt", "hat gehabt", "haben gehabt", "habt gehabt", "haben gehabt", "Irregular german verb."]
+sample_verb = Verb.first
 
+should_generate_present_tense_for_first_verb = sample_verb.presents.create(
+	first_person: sample_present_tense[0],
+	second_person: sample_present_tense[1],
+	third_person: sample_present_tense[2],
+	plural_first_person: sample_present_tense[3],
+	plural_second_person: sample_present_tense[4],
+	plural_third_person: sample_present_tense[5],
+	description: sample_present_tense[6]	
+)
+should_generate_past_tense_for_first_verb = sample_verb.pasts.create(
+	first_person: sample_past_tense[0],
+	second_person: sample_past_tense[1],
+	third_person: sample_past_tense[2],
+	plural_first_person: sample_past_tense[3],
+	plural_second_person: sample_past_tense[4],
+	plural_third_person: sample_past_tense[5],
+	description: sample_past_tense[6]	
+)
+should_generate_perfect_tense_for_first_verb = sample_verb.perfects.create(
+	first_person: sample_perfect_tense[0],
+	second_person: sample_perfect_tense[1],
+	third_person: sample_perfect_tense[2],
+	plural_first_person: sample_perfect_tense[3],
+	plural_second_person: sample_perfect_tense[4],
+	plural_third_person: sample_perfect_tense[5],
+	description: sample_perfect_tense[6]	
+)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+puts "Complete!"
