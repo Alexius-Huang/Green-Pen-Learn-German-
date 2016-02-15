73e109028502a54f7a4045275c6e4c881197e5a4
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-15 22:18:54 +0800

    Able to generate 5 german verbs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 puts "This is the seed file for RAILS application for : green_pen"
 puts "Seed file processing..."
 
-print "Seed file generating 10 users..."
+print "Seed file generating 10 users with 3 admins..."
 
 sample_password = "1234567890"
 sample_email_prefix = ""
 sample_username = ["Max", "Anita", "Mike", "Martin", "Danial", "Sam", "Christina", "Gary", "Ken", "John"]
 
-should_generate_10_users = (1..10).each do |number|
+should_generate_10_users_with_3_admins = (1..10).each do |number|
 	sample_email_prefix += number.to_s
 	sample_email = sample_email_prefix + "@" + sample_email_prefix + ".com"
 	
@@ -27,3 +27,31 @@ should_generate_10_users = (1..10).each do |number|
 end
 
 puts "Complete!"
+
+print "Seed file generating 5 german verbs..."
+
+sample_german_verbs = ["haben", "heißen", "antworten", "gehen", "sehen"]
+sample_verb_description = ["have", "called", "answer", "go to/ come to", "see"]
+
+should_generate_5_german_verbs = (0..4).each do |number|
+	Verb.create(title: sample_german_verbs[number], description: sample_verb_description[number])
+end
+
+print "Complete!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
