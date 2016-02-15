1d71d4ebcdbe1a70b4ea415e19931be2105b1626
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-15 21:44:20 +0800

    Admin role can functioning

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -8,6 +8,18 @@ class AdministratorsController < ApplicationController
 		setup_parameter("admins")
 	end
 
+	def join
+		@user = User.find(params[:id])
+		@user.update(administrator: true)
+		redirect_to administrators_manage_users_path, notice: "User : #{@user.username} Has Successfully Joined to be an Administrator!"
+	end
+
+	def cancel
+		@user = User.find(params[:id])
+		@user.update(administrator: false)
+		redirect_to administrators_manage_admins_path, alert: "User : #{@user.username} Has Changed back to Normal User!"
+	end
+
 	private
 
 	def setup_parameter(type)
