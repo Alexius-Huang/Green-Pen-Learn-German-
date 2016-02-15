801d3f6de953160a495c78c83ec5a73858699e76
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-15 17:47:31 +0800

    Add name column complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
+  protected
 
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email) }
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+  	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password) }
+  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :current_password) }
 	end
 end
