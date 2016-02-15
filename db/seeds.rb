a2cd005529327365870ad08f32045f45560d33e6
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-15 22:07:42 +0800

    Able to auto-generate 10 user accounts with 3 admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "This is the seed file for RAILS application for : green_pen"
+puts "Seed file processing..."
+
+print "Seed file generating 10 users..."
+
+sample_password = "1234567890"
+sample_email_prefix = ""
+sample_username = ["Max", "Anita", "Mike", "Martin", "Danial", "Sam", "Christina", "Gary", "Ken", "John"]
+
+should_generate_10_users = (1..10).each do |number|
+	sample_email_prefix += number.to_s
+	sample_email = sample_email_prefix + "@" + sample_email_prefix + ".com"
+	
+	if (1..3).include? number
+		admin = true
+	else
+		admin = false
+	end
+	
+	User.create(
+		username: sample_username[number - 1],
+		email: sample_email,
+		password: sample_password,
+		password_confirmation: sample_password,
+		administrator: admin
+	)
+end
+
+puts "Complete!"
