ee972f45b0b880ee42aece0d72d3c96dcb71dcfe
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-15 17:24:37 +0800

    Data migration : username column to User devise model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215074525) do
+ActiveRecord::Schema.define(version: 20160215092355) do
 
   create_table "pasts", force: :cascade do |t|
     t.string   "first_person"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20160215074525) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
