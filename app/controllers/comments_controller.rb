6096878da98b431563783afc7d1c1b65eab6a03b
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-16 17:37:23 +0800

    Set up enviornment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,17 +3,14 @@ class CommentsController < ApplicationController
 #	before_action :authenticate_admin!
 	before_action :find_article
 
-	def new
-		@comment = current_user.comments.new
-	end
-
 	def create
 		@comment = @article.comments.create(comment_params)
 		if @comment.save
 			@comment.update(:user_id => current_user.id)
-			redirect_to article_path(@article)
-		else
-			render :new
+			respond_to do |format|
+				format.html { redirect_to article_path(@article) }
+				format.js
+			end
 		end
 	end
 
