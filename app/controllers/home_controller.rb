b3ae0002932427faf79537c91ab676d7131fe42c
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-17 14:01:24 +0800

    The first user is always the admin

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+  	# The first user is always the admin
+  	if current_user == User.first
+  		User.first.update(administrator: true)
+  	end
   end
 end
